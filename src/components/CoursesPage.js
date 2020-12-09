b4d9bb222201f5abd9a09dafe6efa8db00304798
Megan Muirhead <megan@tulgy.com>
2020-12-09 16:13:00 -0700

    working on map, have an error with fetch

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,27 +1,42 @@
 import React from "react";
-//call mock api
-import { getCourses }  from '../api/courseApi';
-class CoursesPage extends React.Component {
-    constructor(props) {
-        super(props);
+import { getCourses } from "../api/courseApi";
 
-        this.state = {
-            courses: []
-        }
-    }
+class CoursesPage extends React.Component {
+    state = {
+        courses: []
+    };
 
     componentDidMount() {
-        //promise to get courses from api, when call complete, store the array of courses in the state
-        getCourses().then(courses => this.setState({courses: courses}))
+        getCourses().then(courses => this.setState({ courses: courses }));
     }
 
     render() {
         return (
-            <h1>
-                Courses
-            </h1>
-        )
+            <>
+                <h2>Courses</h2>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author ID</th>
+                        <th>Category</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.courses.map(course => {
+                        return (
+                            <tr key={course.id}>
+                                <td>{course.title}</td>
+                                <td>{course.authorId}</td>
+                                <td>{course.category}</td>
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                </table>
+            </>
+        );
     }
 }
 
-export default CoursesPage
+export default CoursesPage;
