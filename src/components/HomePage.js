8360c485fd9f409f662d86f4f09a89132557788f
Megan Muirhead <megan@tulgy.com>
2020-12-09 14:12:21 -0700

    working on react site, all the start up files and env set up

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function HomePage() {
+
+    return (
+        <div className='jumbotron'>
+            <h1>Megan's Site</h1>
+            <p>This app has react, flux and react router for ultra-responsive web apps!</p>
+        </div>
+    );
+}
+
+export default HomePage;
