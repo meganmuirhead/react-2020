dc5d16143ff5fa052c4699be9fd63b34fa57bb1a
Megan Muirhead <megan@tulgy.com>
2020-12-09 15:05:36 -0700

    working on components

---
+++ src/components/common/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+
+   return (
+       <nav>
+           <a href="/">Home Page</a> | <a href="/about">About Page</a>
+
+       </nav>
+   )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import {render} from 'react-dom';
-import HomePage from "./components/HomePage";
+import App from "./components/App";
 
 //entry point for app
 
-render(<HomePage />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
