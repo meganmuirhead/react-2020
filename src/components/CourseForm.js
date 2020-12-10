867a130efa5c3fcbe7ddd8e678326cecba6cc6df
Megan Muirhead <megan@tulgy.com>
2020-12-09 20:33:56 -0700

    working on form

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function CourseForm(props) {
+
     return (
         <form>
             <div className="form-group">
@@ -11,7 +12,8 @@ function CourseForm(props) {
                         type="text"
                         name="title"
                         className="form-control"
-                        value=""
+                        onChange={props.onTitleChange}
+                        value={props.course.title}
                     />
                 </div>
             </div>
@@ -22,10 +24,10 @@ function CourseForm(props) {
                     <select
                         id="author"
                         name="authorId"
-                        value=""
+                        value={props.course.authorId || ''}
                         className="form-control"
                     >
-                        <option value="" />
+                        <option value=""/>
                         <option value="1">Cory House</option>
                         <option value="2">Scott Allen</option>
                     </select>
@@ -40,12 +42,12 @@ function CourseForm(props) {
                         id="category"
                         name="category"
                         className="form-control"
-                        value=""
+                        value={props.course.category}
                     />
                 </div>
             </div>
 
-            <input type="submit" value="Save" className="btn btn-primary" />
+            <input type="submit" value="Save" className="btn btn-primary"/>
         </form>
     );
 }
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import { Prompt } from "react-router-dom";
 import CourseForm from "./CourseForm";
 
 
 const ManageCoursePage = (props) => {
 // debugger;
+    const [courseInput, setCourseInput] = useState({
+        id: null,
+        slug: '',
+        authorId: '',
+        category: ''
+    });
+
+    function handleTitleChange(e) {
+        const updatedCourse = {...courseInput, title: e.target.value}
+        setCourseInput(updatedCourse)
+    }
     return (
         <>
              <h2> Manage Course</h2>
             {/*<Prompt when={true} message="Are you sure you want to leave this page?"></Prompt>*/}
             {/*{props.match.params.slug}*/}
             <p>React is awesome!</p>
-            <CourseForm />
+            <CourseForm course={courseInput} onTitleChange={handleTitleChange}/>
         </>
     )
 };
