4944b3f4575702f4bdac1a8a6435bfa66cf02192
Megan Muirhead <megan@tulgy.com>
2020-12-09 19:25:48 -0700

    added url params

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 function CourseList(props) {
 
     return (
@@ -16,7 +17,7 @@ function CourseList(props) {
             {props.courses.map(course => {
                 return (
                     <tr key={course.id}>
-                        <td>{course.title}</td>
+                        <td><Link to={"/course/" + course.slug}>{course.title}</Link></td>
                         <td>{course.authorId}</td>
                         <td>{course.category}</td>
                     </tr>
@@ -25,18 +26,19 @@ function CourseList(props) {
             </tbody>
         </table>
     )
-}
 
 //adding property to function to validate props being passed in
 //typing the shape of array
 
-CourseList.propTypes = {
-    courses: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        authorId: PropTypes.number.isRequired,
-        category: PropTypes.string.isRequired
-})).isRequired
-};
+    CourseList.propTypes = {
+        courses: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            authorId: PropTypes.number.isRequired,
+            category: PropTypes.string.isRequired
+        })).isRequired
+    };
+}
+
 
 export default CourseList;
