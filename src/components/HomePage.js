152788691803693afba08af1e8b9b390ae16632b
Megan Muirhead <megan@tulgy.com>
2020-12-09 18:45:52 -0700

    adding link

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 function HomePage() {
@@ -7,6 +8,7 @@ function HomePage() {
         <div className='jumbotron'>
             <h1>Megan's Site</h1>
             <p>This app has react, flux and react router for ultra-responsive web apps!</p>
+            <Link to='about' className="btn btn-primary">About</Link>
         </div>
     );
 }
