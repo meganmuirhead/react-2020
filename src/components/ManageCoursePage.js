b89b46b041d0150e25d315e6a66edcae3d620328
Megan Muirhead <megan@tulgy.com>
2020-12-09 19:30:17 -0700

    adding prompts

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Prompt } from "react-router-dom";
+
 
 const ManageCoursePage = (props) => {
 // debugger;
     return (
         <>
              <h2> Manage Course</h2>
-            {props.match.params.slug}
+            <Prompt when={true} message="Are you sure you want to leave this page?"></Prompt>
+            {/*{props.match.params.slug}*/}
         </>
     )
-}
+};
 
 export default ManageCoursePage;
